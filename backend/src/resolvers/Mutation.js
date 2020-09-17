1035fcc426692b4e23d99f9bb55804b0ef36a9c0
Saiful <msisaifulsaif@gmail.com>
2020-09-17 11:51:53 +0600

    add to cart item stuffs

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -226,8 +226,36 @@ const Mutations = {
         }
       }, info
     );
-  }
-  
+  },
+  async addToCart(paent, args, ctx, info){
+    const { userId } = ctx.request;
+    if (!userId) {
+      throw new Error("You must be logged in to do that")
+    }
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      user: { id: userId },
+      item: { id: args.id },
+    })
+
+    if (existingCartItem) {
+      return ctx.db.mutation.updateCartItem({
+        where: { id: existingCartItem.id },
+        data: { quantity: existingCartItem.quantity + 1 },
+        info
+      });
+    }
+
+    return ctx.db.mutation.createCartItem({
+      data: { 
+        user: {
+          connect: { id: userId }
+        },
+        item: {
+          connect: { id: args.id }
+        },        
+      }, info
+    });
+  }  
 };
 
 module.exports = Mutations;
